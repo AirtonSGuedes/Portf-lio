a945a6c54c846d991faca902c9f9e11f82790091
AirtonSGuedes <113541135+AirtonSGuedes@users.noreply.github.com>
2022-09-30 16:44:14 -0300

    pasta

---
+++ projeto teste - Copia/java.js
@@ -0,0 +1,28 @@
+
+
+var foto = document.getElementById("A26")
+var button = document.getElementById("airton");
+
+button.addEventListener("click", function(){
+
+    var foto = document.getElementById("A26")
+
+    if(foto.style.display === "block") {
+        foto.style.display = "none" ;
+    } else {
+        foto.style.display = "block"
+    }
+})
+var foto = document.getElementById("guedes")
+var button = document.getElementById("airton");
+
+button.addEventListener("click", function(){
+
+    var foto = document.getElementById("guedes")
+
+    if(foto.style.display === "none") {
+        foto.style.display = "block" ;
+    } else {
+        foto.style.display = "none"
+    }
+})
